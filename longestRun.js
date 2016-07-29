a47dcd1a0b09458ddd23179a4a3329f1253cfba8
ckim16 <aero.cheewoon@gmail.com>
2016-07-28 23:06:09 -0700

    longest run

--- longestRun.js
+++ longestRun.js
@@ -1,14 +1,24 @@
 'use strict';
 function longestRun(str) {
-  var modifiedStr = str.split('');
-  for(var i = 0; i < modifiedStr.length; i++) {
-    if(modifiedStr[i] !== '/') {
-      if(modifiedStr[i] !== modifiedStr[i+1] && modifiedStr[i+1] !== undefined) {
-        modifiedStr.splice(i+1, 0, '/');
-      }
+  var arr = [];
+  var letters = '';
+  for(var i = 0; i < str.length; i++) {
+    if(str.charAt(i) !== str.charAt(i+1)) {
+      arr.push(letters + str.charAt(i));
+      letters = '';
+    } else {
+      letters += str.charAt(i);
     }
   }
-  var arr = modifiedStr.join('').split('/');
+  // var modifiedStr = str.split('');
+  // for(var i = 0; i < modifiedStr.length; i++) {
+  //   if(modifiedStr[i] !== '/') {
+  //     if(modifiedStr[i] !== modifiedStr[i+1] && modifiedStr[i+1] !== undefined) {
+  //       modifiedStr.splice(i+1, 0, '/');
+  //     }
+  //   }
+  // }
+
   var longest = 0;
   var longestLetter;
   for(var i = 0; i < arr.length; i++) {
@@ -17,7 +27,7 @@ function longestRun(str) {
       longestLetter = arr[i];
     }
   }
-  console.log( [str.indexOf(longestLetter[0]), str.indexOf(longestLetter[0])+ longest-1])
+  console.log([str.indexOf(longestLetter[0]), str.indexOf(longestLetter[0])+longest-1])
   return [str.indexOf(longestLetter[0]), str.indexOf(longestLetter[0])+longest-1]
 }
 longestRun("abbbcc")
