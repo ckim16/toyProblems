9330b85cf5dc4778563e367ebda062e76a307eab
ckim16 <aero.cheewoon@gmail.com>
2016-07-28 23:07:06 -0700

    longest run

--- longestRun.js
+++ longestRun.js
@@ -10,14 +10,6 @@ function longestRun(str) {
       letters += str.charAt(i);
     }
   }
-  // var modifiedStr = str.split('');
-  // for(var i = 0; i < modifiedStr.length; i++) {
-  //   if(modifiedStr[i] !== '/') {
-  //     if(modifiedStr[i] !== modifiedStr[i+1] && modifiedStr[i+1] !== undefined) {
-  //       modifiedStr.splice(i+1, 0, '/');
-  //     }
-  //   }
-  // }
 
   var longest = 0;
   var longestLetter;
@@ -27,7 +19,5 @@ function longestRun(str) {
       longestLetter = arr[i];
     }
   }
-  console.log([str.indexOf(longestLetter[0]), str.indexOf(longestLetter[0])+longest-1])
   return [str.indexOf(longestLetter[0]), str.indexOf(longestLetter[0])+longest-1]
 }
-longestRun("abbbcc")
