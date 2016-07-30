6d8d22fe8667eef3824bbf42edfde9956eb225f8
ckim16 <aero.cheewoon@gmail.com>
2016-07-29 19:23:27 -0700

    character frequency

---
+++ characterFrequency.js
@@ -0,0 +1,26 @@
+'use strict';
+function characterFrequency(str) {
+  var arr = str.split('');
+  var sorted = arr.reduce(function(accum, value) {
+    if(!accum[value]) {
+      accum[value] = 1;
+    } else {
+      accum[value] += 1;
+    }
+    return accum;
+  }, {});
+  
+  var result = [];
+  for(var key in sorted) {
+    result.push([key, sorted[key]]);
+  }
+  result.sort(function(a, b) {
+    if(a[1] === b[1]) {
+      return a[0] - b[0];
+    } else {
+      return b[1] - a[1];
+    }
+  });
+}
+
+characterFrequency('')
