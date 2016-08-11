2feed27714a62cb04ba0597c8ef007582680eaa7
ckim16 <aero.cheewoon@gmail.com>
2016-08-10 17:42:57 -0700

    Compose and Pipe

---
+++ composeNPipe.js
@@ -0,0 +1,18 @@
+'use strict';
+var compose = function() {
+  var fns = Array.prototype.slice.call(arguments);
+  return function(val) {
+    return fns.reduceRight(function(prev, next) {
+      return next(prev);
+    }, val);    
+  };
+};
+
+var pipe = function() {
+  var fns = Array.prototype.slice.call(arguments);
+  return function(val) {
+    return fns.reduce(function(prev, next) {
+      return next(prev); 
+    }, val);
+  }
+};
