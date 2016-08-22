20f11e8fe3e5ba89ce2484f91f064f7200f45879
ckim16 <aero.cheewoon@gmail.com>
2016-08-22 11:06:05 -0700

    fraction converter

--- fractionCoverter.js
+++ fractionCoverter.js
@@ -1,22 +1,11 @@
 'use strict';
 function fractionConverter(num) {
-  var numStr = num.toString();
-  if(numStr.indexOf('.') === -1) {
-    return numStr + '/1';
-  }
-  var digitArr = numStr.split('.');
-  var number = digitArr[0];
-  var fraction = digitArr[1];
+  var denominator = 0;
 
-  var gcd = function(a, b) {
-    if(!b) {
-      return a;
+  while(true) {
+    if(Math.round(num*denominator)/denominator === num) {
+      return Math.round(num*denominator) + '/' + denominator;
     }
-    return gcd(b, a%b);
-  };
-
-  var greatestDivisor = gcd(fraction, Math.pow(10, fraction.length));
-  var denominator = Math.pow(10, fraction.length) / greatestDivisor;
-  var numerator = number*denominator + (fraction/greatestDivisor);
-  return numerator + '/' + denominator;
+    denominator++;
+  }
 }
