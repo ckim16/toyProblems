601cf7f803c261ab0ffbef18360a60acbeacd6c3
ckim16 <aero.cheewoon@gmail.com>
2016-12-08 13:50:47 -0800

    Update Non Repeated Characters

--- nonRepeatedCharacters.js
+++ nonRepeatedCharacters.js
@@ -19,3 +19,24 @@ function nonRepeatedCharacters(string) {
   }
   return 'sorry';
 }
+
+// different solution
+
+function nrcOpt(str) {
+  var startIdx = 0;
+  var endIdx = str.length-1;
+  var storage = [];
+
+  while (startIdx < endIdx && startIdx < Math.floor(str.length/2)) {
+    if (str.charAt(startIdx) === str.charAt(endIdx)) {
+      startIdx++;
+      endIdx = str.length-1;
+      storage = [];
+    } else {
+      storage.push(str.charAt(startIdx));
+      endIdx--;
+    }
+  }
+  console.log('storage', storage);
+  return storage[0] || 'sorry';
+}
